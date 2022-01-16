38ede923338db75dafdc766fbb450b671843f1b0
Tanya Zhuge <tangnazhuge@gmail.com>
2022-01-16 09:27:23 -0800

    Add Cart functionality

--- src/Components/Card/Card.js
+++ src/Components/Card/Card.js
@@ -1,10 +1,15 @@
 import React, { useState, useEffect }from 'react';
 import './Card.scss';
 
-const Card = () => {
+const Card = ({id, earth_date, sol, camera, roverName, url}) => {
+
   return (
     <div className="card">
-        
+      <img className='image-card' src={url} alt={roverName,camera} id={id}/>
+      <h1>{roverName + ` Rover - ` + camera }</h1>  
+      <p>sol: {sol}</p>
+      <p>date: {earth_date}</p>
+      <button>Like</button>
     </div>
   );
 }
